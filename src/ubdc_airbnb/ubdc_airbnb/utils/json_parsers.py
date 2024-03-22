9c90ba4fb8d9eb250b3032bfe4f19a5e8cb0e7c3
nikos ves <nick.ves@glasgow.ac.uk>
2024-03-22 18:35:41 +0000

    Add JSON parsing functionality to airbnb_response_parser class

--- src/ubdc_airbnb/ubdc_airbnb/utils/json_parsers.py
+++ src/ubdc_airbnb/ubdc_airbnb/utils/json_parsers.py
@@ -1,12 +1,29 @@
+from typing import Any, Iterator
+
 from jsonpath_ng import parse
 
 
 # noinspection PyPep8Naming
 class airbnb_response_parser:
+    __primary_host__ = parse(r"$..primary_host")
+    __additional_hosts__ = parse(r"$..additional_hosts")
+    __user_object__ = parse(r"$..user")
     __profile_pics_parser__ = parse(r"$.user.[picture_url,picture_url_large,picture_url,thumbnail_url]")
     __listings_count__ = parse(r"$..listings_count")
     __price_histogram__ = parse(r"$..price_histogram.histogram")
     __has_next_page__ = parse(r"$..pagination_metadata.has_next_page")
+
+    @staticmethod
+    def generic(pattern: str, target: dict) -> Iterator[Any]:
+        parser = parse(pattern)
+        matches = parser.find(target)
+        for match in matches:
+            yield match.value
+
+    @classmethod
+    def get_user_object(cls, response: dict) -> list[dict]:
+        matches = cls.__user_object__.find(response)
+        return [m.value for m in matches]
 
     @classmethod
     def has_next_page(cls, response: dict) -> bool:
