2cd1dcf05a19d4aa43ee57fe8ea7977c57b849a0
Nikos Ves <Nick.Ves@glasgow.ac.uk>
2025-01-23 12:02:09 +0000

    Added new task: task_get_next_page_homes + supporting function  + tests

--- tests/workflows/test_calendar_workflows.py
+++ tests/workflows/test_calendar_workflows.py
@@ -1,30 +1,61 @@
 from typing import TYPE_CHECKING
-
+from collections import namedtuple
 import pytest
 from celery.result import GroupResult
 
+response_data = namedtuple(
+    typename="response_data",
+    field_names=["status_code", "body", "headers",],
+    defaults=[200, {}, {}]
+)
+
 
 @pytest.mark.parametrize(
-    "listing_ids",
+    "listing_id,responses",
     [
-        (900, 9001),
-        (800, 80000000000001),
+        # listing_id, responses
+        (900,            [response_data()]),
+        (1316740343258461084, [response_data()]),  # a very large (real) listing_id
+        (900.0,           [response_data()]),  # listing_id as float.
+        (900,            [response_data(403)]),  # Forbidden
+        (900,            [  # first response is 429,then retry, then 200
+            response_data(429, {}, {}),  # Too many requests
+            response_data(200, {}, {}),  # then ok
+        ]),
+        (900,            [  # first response is 429,then retry, then 200
+            response_data(429, {}, {}),  # Too many requests x 3 times;  fail
+            response_data(429, {}, {}),
+            response_data(429, {}, {}),
+        ]),
+        (900,            [
+            response_data(503, headers={"X-Crawlera-Error": "banned"})],
+         ),
     ],
 )
 @pytest.mark.django_db(transaction=True)
 def test_op_update_calendars_for_listing_ids(
     mock_airbnb_client,
+    response_queue,
+    responses: list[response_data],
     listings_model,
     celery_worker,
     celery_app,
-    listing_ids,
+    listing_id,
 ):
-    """Given a set of listing, run the workflow to get their calendars."""
+    """Given a listing, run the workflow to get their calendars."""
 
-    from ubdc_airbnb.models import AirBnBResponse
     from ubdc_airbnb.operations import op_update_calendars_for_listing_ids
 
-    task = op_update_calendars_for_listing_ids.s([x for x in listing_ids])
+    for rq in responses:
+        response_data = {}
+        response_data.update(
+            listing_id=listing_id,
+            status_code=rq.status_code,
+            headers=rq.headers,
+        )
+        response_queue.put(response_data)
+
+    task = op_update_calendars_for_listing_ids.s([listing_id])
     # fire the task
     result = task.apply_async()
 
@@ -34,21 +65,20 @@ def test_op_update_calendars_for_listing_ids(
     group_result = GroupResult.restore(group_result_id)  # type: ignore
     assert group_result is not None
     assert group_result.children is not None
-    ## assert job.children is populated
+    # assert job.children is populated
 
-    ## join_native does not support django db backend
+    # join_native does not support django db backend
     g = group_result.join(propagate=False)
 
-    r = AirBnBResponse.objects.filter(listing_id__in=listing_ids)
-    assert r.count() == 2
-    assert r.filter(status_code=200).count() == 2
-    assert r.filter(_type="CAL").count() == 2
-    assert r.filter(ubdc_task__status="SUCCESS").count() == 2
-    for ar in r:
-        ubdc_task = ar.ubdc_task
-        assert ubdc_task is not None  #
-        listing_id_str = str(ar.listing_id)
-        if listing_id_str == "800":
-            assert ubdc_task.retries == 2
-        else:
-            assert ubdc_task.retries == 0
+    from ubdc_airbnb.models import AirBnBResponse
+
+    # regarlless of the number of responses, the last one is the one that counts
+    expected_status_code = [r.status_code for r in responses][-1]
+
+    rf = AirBnBResponse.objects.filter(listing_id=listing_id)
+    assert rf.count() == 1
+    r: AirBnBResponse = rf.first()  # type: ignore
+    assert r.status_code == expected_status_code
+    from uuid import UUID
+    assert r.ubdc_task.group_task_id == UUID(group_result_id)  # type: ignore
+    assert r._type == 'CAL'
