a933183beca4fc73ef72ce836206860f6370df27
Nikos Ves <nick.ves@glasgow.ac.uk>
2025-01-26 11:19:35 +0000

    [Refactor] Refactored Test Suite

---
+++ tests/beat_ops/test_op_discover_new_listings_periodical.py
@@ -0,0 +1,57 @@
+import pytest
+from django.core.management import call_command
+from faker import Faker
+
+from ..payload_generators import search_body_generator
+
+fake = Faker()
+
+
+@pytest.mark.django_db(transaction=True)
+def test_op_discover_new_listings_periodical(
+    mock_airbnb_client,
+    response_queue,
+    celery_worker,
+    celery_app,
+    listings_model,
+    ubdcgrid_model,
+    ubdctask_model,
+    responses_model,
+):
+    from ubdc_airbnb.operations.discovery import op_discover_new_listings_periodical
+
+    for idx in range(0, 4):
+        qk = "03113322331322" + str(idx)
+        payload = {
+            "content": search_body_generator(qk=qk),
+        }
+        response_queue.put(payload)
+        call_command("create-test-area", qk)
+
+    grid = ubdcgrid_model.objects.all()
+    for idx, g in enumerate(grid):
+        centroid = g.geom_3857.centroid
+        listing = listings_model.objects.create(listing_id=idx + 1, geom_3857=centroid)
+
+    task = op_discover_new_listings_periodical.s()
+    job = task.apply_async()
+    result = job.get()
+    assert job.children
+    group_results = [c for c in job.children if c.__class__.__name__ == "GroupResult"]
+    for g in group_results:
+        g.join()  # type: ignore
+
+    assert job.successful()
+    assert responses_model.objects.count() == 4
+    for l in listings_model.objects.all():
+        assert l.timestamp
+        assert l.listing_updated_at is None
+        assert l.calendar_updated_at is None
+        assert l.listing_updated_at is None
+        assert l.reviews_updated_at is None
+        assert l.booking_quote_updated_at is None
+
+    for g in ubdcgrid_model.objects.all():
+        assert g.timestamp
+        assert g.datetime_last_listings_scan
+        assert g.datetime_last_listings_scan > g.timestamp
--- tests/operations/op_update_calendars_for_listing_ids.py
+++ tests/operations/op_update_calendars_for_listing_ids.py
@@ -1,12 +1,17 @@
-from typing import TYPE_CHECKING
 from collections import namedtuple
+from typing import TYPE_CHECKING
+
 import pytest
 from celery.result import GroupResult
 
 response_data = namedtuple(
     typename="response_data",
-    field_names=["status_code", "body", "headers",],
-    defaults=[200, {}, {}]
+    field_names=[
+        "status_code",
+        "body",
+        "headers",
+    ],
+    defaults=[200, {}, {}],
 )
 
 
@@ -14,22 +19,29 @@ response_data = namedtuple(
     "listing_id,responses",
     [
         # listing_id, responses
-        (900,            [response_data()]),
+        (900, [response_data()]),
         (1316740343258461084, [response_data()]),  # a very large (real) listing_id
-        (900.0,           [response_data()]),  # listing_id as float.
-        (900,            [response_data(403)]),  # Forbidden
-        (900,            [  # first response is 429,then retry, then 200
-            response_data(429, {}, {}),  # Too many requests
-            response_data(200, {}, {}),  # then ok
-        ]),
-        (900,            [  # first response is 429,then retry, then 200
-            response_data(429, {}, {}),  # Too many requests x 3 times;  fail
-            response_data(429, {}, {}),
-            response_data(429, {}, {}),
-        ]),
-        (900,            [
-            response_data(503, headers={"X-Crawlera-Error": "banned"})],
-         ),
+        (900.0, [response_data()]),  # listing_id as float.
+        (900, [response_data(403)]),  # Forbidden
+        (
+            900,
+            [  # first response is 429,then retry, then 200
+                response_data(429, {}, {}),  # Too many requests
+                response_data(200, {}, {}),  # then ok
+            ],
+        ),
+        (
+            900,
+            [  # first response is 429,then retry, then 200
+                response_data(429, {}, {}),  # Too many requests x 3 times;  fail
+                response_data(429, {}, {}),
+                response_data(429, {}, {}),
+            ],
+        ),
+        (
+            900,
+            [response_data(503, headers={"X-Crawlera-Error": "banned"})],
+        ),
     ],
 )
 @pytest.mark.django_db(transaction=True)
@@ -80,5 +92,6 @@ def test_op_update_calendars_for_listing_ids(
     r: AirBnBResponse = rf.first()  # type: ignore
     assert r.status_code == expected_status_code
     from uuid import UUID
+
     assert r.ubdc_task.group_task_id == UUID(group_result_id)  # type: ignore
-    assert r._type == 'CAL'
+    assert r._type == "CAL"
